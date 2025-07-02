ca780e52236367426dddc1341d85236a0117cb75
CalonPeretas <buyuangpalala41@gmail.com>
2025-07-02 15:31:52 +0700

    Perbaiki isi class migrasi pembukuans

--- database/migrations/2025_06_29_164144_create_pembukuans_table.php
+++ database/migrations/2025_06_29_164144_create_pembukuans_table.php
@@ -1,8 +1,25 @@
-Schema::create('pembukuans', function (Blueprint $table) {
-    $table->id();
-    $table->enum('jenis', ['Pemasukan', 'Pengeluaran']);
-    $table->string('keterangan');
-    $table->decimal('jumlah', 15, 2);
-    $table->date('tanggal');
-    $table->timestamps();
-});
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembukuansTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('pembukuans', function (Blueprint $table) {
+            $table->id();
+            $table->enum('jenis', ['Pemasukan', 'Pengeluaran']);
+            $table->string('keterangan');
+            $table->decimal('jumlah', 15, 2);
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pembukuans');
+    }
+}
