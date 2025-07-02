306c56827cf9b3fd0520690c2caf81cc9072e597
CalonPeretas <buyuangpalala41@gmail.com>
2025-07-02 16:10:58 +0700

    Tambah start.sh untuk jalankan Laravel di Railway

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-# Jalankan migration jika perlu
 php artisan migrate --force
-
-# Jalankan Laravel server
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 php artisan serve --host=0.0.0.0 --port=8080
